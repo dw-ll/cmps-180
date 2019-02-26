08769898fc436bf3cb5d6bcf571583fd58db3f63
Daniel Williams <danswilliams95@gmail.com>
2019-02-26 15:35:40 -0800

    currently finds ONE minimum ts.

--- lab3/createview.sql
+++ lab3/createview.sql
@@ -1,12 +1,16 @@
-CREATE VIEW openPrice AS
-SELECT exchangeID, symbol, quoteTS, price, DATE(quoteTS)
-FROM Quotes 
-WHERE quoteTS::TIME IN(SELECT MIN(q.quoteTS::TIME) FROM Quotes q WHERE exchangeID=q.exchangeID AND  AND DATE(q.quoteTS)=DATE(quoteTS) );
+--CREATE VIEW openPrice AS
+--SELECT exchangeID, symbol, quoteTS, price, DATE(quoteTS)
+--FROM Quotes 
+--WHERE quoteTS::TIME IN(SELECT MIN(qquoteTS::TIME) FROM Quotes q WHERE exchangeID=qexchangeID AND  AND DATE(qquoteTS)=DATE(quoteTS) );
 
 
 CREATE VIEW QuotesSummary AS
-SELECT exchangeID, symbol,quoteTS::DATE as theDate, MIN(price) as lowPrice, MAX(price) as highPrice
-FROM Quotes
+SELECT exchangeID, symbol,quoteTS::DATE as theDate, MIN(price) as lowPrice, MAX(price) as highPrice,
+(SELECT MIN(q2.quoteTS::TIME)
+FROM Quotes q2 
+WHERE exchangeID = q2.exchangeID AND q2.symbol = symbol AND q2.quoteTS::DATE=quoteTS::DATE
+ORDER BY MIN(q2.quoteTS::TIME)) openingPrice
+FROM Quotes 
 GROUP BY exchangeID, symbol, theDate
 ORDER BY theDate;
 
