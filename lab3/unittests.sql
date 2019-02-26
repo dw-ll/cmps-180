57bf1d5b71f2324d3e9e914b086f670284c756e9
Daniel Williams <danswilliams95@gmail.com>
2019-02-25 23:31:20 -0800

    adding unit tests for general constraints.

--- lab3/unittests.sql
+++ lab3/unittests.sql
@@ -4,6 +4,34 @@ INSERT INTO Trades (exchangeID,symbol,tradeTS,buyerID,sellerID,price,volume)
 INSERT INTO Trades (exchangeID,symbol,tradeTS,buyerID,sellerID,price,volume)
             VALUES ('NYSE','CLDR','2018-11-20 13:03:24',1489,1101,10,1000);
 INSERT INTO Trades (exchangeID,symbol,tradeTS,buyerID,sellerID,price,volume)
-            VALUES ('WOMBT','AAPL','2018-11-20 13:03:24',1489,1456,10,1000);
+            VALUES ('WOMBT','RBT','2018-11-20 13:03:24',1489,1456,10,1000);
 INSERT INTO Quotes (exchangeID,symbol,quoteTS,price)
-            VALUES ('NYSE','RBT','2018-11-20 13:03:24',5000);
+            VALUES ('CAML','RBT','2018-11-20 13:03:24',5000);
+-- General Constraint Unit Tests (1 Pass 1 Fail)
+-- pos_price
+UPDATE Quotes
+set price =50;
+UPDATE Quotes
+set price =-1;
+
+-- pos_cost
+UPDATE Trades
+set price =1, volume =2;
+UPDATE Trades
+set price = 0, volume = 100;
+
+-- diff_id
+UPDATE Trades
+set buyerID = 1, sellerID = 2;
+UPDATE Trades 
+set buyerID = 1, sellerID = 1;
+
+-- h_flag
+UPDATE Customers
+set isValidCustomer=true
+WHERE (category = 'H');
+UPDATE Customers
+set isValidCustomer=false
+WHERE (category = 'H');
+
+
