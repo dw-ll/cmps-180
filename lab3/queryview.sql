723a49109483bb0e8c920d445748e08b5300fd1b
Daniel Williams <danswilliams95@gmail.com>
2019-02-26 19:08:18 -0800

    queryview is complete.

---
+++ lab3/queryview.sql
@@ -0,0 +1,33 @@
+-- A query to determine how many times a stock closes on it's high price
+-- on a certain date. Tables used: Exchanges, Stocks || Views used: QuotesSummary
+
+SELECT e.exchangeName,s.stockName, COUNT(q.theDate) as numHighClosing
+FROM Exchanges e, Stocks s, QuotesSummary q
+WHERE e.exchangeID = s.exchangeID AND q.symbol = s.symbol
+AND (q.highPrice = q.closingPrice)
+GROUP BY e.exchangeName , s.stockName
+HAVING COUNT(q.theDate) >=2;
+-- Output
+--      exchangename       |           stockname            | numhighclosing
+-------------------------+--------------------------------+----------------
+-- NASDAQ Stock Exchange   | Cloudera                       |              3
+-- NASDAQ Stock Exchange   | Hewlett Packard Enterprise Co. |              2
+-- New York Stock Exchange | Cloudera,Inc.                  |              3
+-- New York Stock Exchange | HP Enterprise                  |              2
+
+-- Delete of two possible tuples in Quotes.
+DELETE 
+FROM Quotes
+WHERE exchangeID = 'NYSE'
+AND symbol = 'CLDR';
+
+DELETE
+FROM Quotes
+WHERE exchangeID = 'NASDAQ'
+AND symbol = 'ANF';
+
+-- Output of queryview.sql after DELETE statements.
+--      exchangename       |           stockname            | numhighclosing
+-------------------------+--------------------------------+----------------
+-- NASDAQ Stock Exchange   | Hewlett Packard Enterprise Co. |              2
+--  New York Stock Exchange | HP Enterprise                  |              2
