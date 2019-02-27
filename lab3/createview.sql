213be4d188a43b56dace0b22c285fd586b345d2b
Daniel Williams <danswilliams95@gmail.com>
2019-02-26 18:30:30 -0800

    createview is complete.

--- lab3/createview.sql
+++ lab3/createview.sql
@@ -1,16 +1,30 @@
---CREATE VIEW openPrice AS
---SELECT exchangeID, symbol, quoteTS, price, DATE(quoteTS)
---FROM Quotes 
---WHERE quoteTS::TIME IN(SELECT MIN(qquoteTS::TIME) FROM Quotes q WHERE exchangeID=qexchangeID AND  AND DATE(qquoteTS)=DATE(quoteTS) );
-
-
-CREATE VIEW QuotesSummary AS
-SELECT exchangeID, symbol,quoteTS::DATE as theDate, MIN(price) as lowPrice, MAX(price) as highPrice,
-(SELECT MIN(q2.quoteTS::TIME)
-FROM Quotes q2 
-WHERE exchangeID = q2.exchangeID AND q2.symbol = symbol AND q2.quoteTS::DATE=quoteTS::DATE
-ORDER BY MIN(q2.quoteTS::TIME)) openingPrice
+-- Helper view to obtain opening price for a stock on each day it traded.
+CREATE VIEW openPrice AS
+SELECT  exchangeID, symbol, quoteTS, price, DATE(quoteTS)
 FROM Quotes 
-GROUP BY exchangeID, symbol, theDate
+WHERE quoteTS::TIME IN (SELECT  MIN(q.quoteTS::TIME) 
+                        FROM Quotes q
+                         WHERE exchangeID=q.exchangeID AND DATE(q.quoteTS)=DATE(quoteTS)
+                         GROUP BY exchangeID, symbol,DATE(quoteTS)  );
+
+-- Helper view to obtain closing price for a stock on each day it traded.
+CREATE VIEW closePrice AS
+SELECT  exchangeID, symbol, quoteTS, price, DATE(quoteTS)
+FROM Quotes 
+WHERE quoteTS::TIME IN (SELECT  MAX(q.quoteTS::TIME) 
+                        FROM Quotes q
+                         WHERE exchangeID=q.exchangeID AND DATE(q.quoteTS)=DATE(quoteTS)
+                         GROUP BY exchangeID, symbol,DATE(quoteTS));
+
+-- View that displays the following: exchangeID, stock symbol, date traded, lowest price for that stock, 
+-- highest price for that stock, opening price for that stock on that day, and closing price for that stock
+-- on that day.
+CREATE VIEW QuotesSummary AS
+SELECT DISTINCT q.exchangeID, q.symbol,q.quoteTS::DATE as theDate, MIN(q.price) as lowPrice, MAX(q.price) as highPrice,
+p.price AS openingPrice, c.price AS closingPrice
+FROM Quotes q, openPrice p, closePrice c 
+WHERE DATE(p.quoteTS) = DATE(q.quoteTS) AND DATE(c.quoteTS) = DATE(q.quoteTS) AND p.exchangeID = q.exchangeID 
+AND p.exchangeID = c.exchangeID AND q.exchangeID = c.exchangeID
+GROUP BY q.exchangeID, q.symbol, theDate, openingPrice, closingPrice
 ORDER BY theDate;
 
