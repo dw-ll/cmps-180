916f30342f55ad29c5b2baccc5eb78d5940c709b
dw-ll <danswilliams95@gmail.com>
2019-03-08 12:54:12 -0800

    Delete dblog.sh

--- lab_4/dblog.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect -f
-eval spawn psql -h cmps180-db.lt.ucsc.edu -U daswilli
-set prompt "Password for user daswilli:"
-interact -o -nobuffer -re $prompt return
-send "function02manager\r"
-interact
